c8cc6f713e48b79d074b1d268d86913b7a4d000c
Marina Maslova <msg4marina@gmail.com>
2021-05-10 23:08:38 +0300

    1.The variable "String notification" has been changed to  "boolean notification") 2.xpath("(//div)[3]" has been changed to xpath("//div[contains(text(),'Action successful')]") 3.The method "getText()" has changed to "isDisplayed()" 4.assertEquals has been changed to  Assert.assertTrue. Conclusion: The test passed

--- src/test/java/NotificationMessagesTest.java
+++ src/test/java/NotificationMessagesTest.java
@@ -1,18 +1,18 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
 
 public class NotificationMessagesTest extends BaseTest {
 
     @Test
     public void clickTheButtonAndWaitTheNotificationMessage (){
         driver.get("http://the-internet.herokuapp.com/notification_message_rendered");
-        WebElement m1 = driver.findElement(By.xpath("//a[@href='/notification_message']"));
-        m1.click();
-        String notification = driver.findElement(By.xpath("(//div)[3]")).getText();
-        assertEquals(notification, "Action successful",
-              "Bug in code.Notification message:Action successful or Action unsuccessful " +
-                      "will be changed after reload of the page");
+        WebElement button = driver.findElement(By.xpath("//a[@href='/notification_message']"));
+        button.click();
+        boolean notification = driver.findElement(By.xpath("//div[contains(text(),'Action successful')]")).isDisplayed();
+        Assert.assertTrue(notification, "Action successful " +
+                "Bug in code.Notification message:Action successful or Action unsuccessful " +
+                "will be changed after reload of the page");
     }
 }
